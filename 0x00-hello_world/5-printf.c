4d50efffa52f9d413bbd7e7a151773cd1381148f
Richard Dzadey <rye2khosby@gmail.com>
2022-09-08 14:42:09 -0700

    Hello World C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,14 @@
 #!/bin/bash
+#include <stdio.h>
 
+/**
+ *  * main - Entry point of code
+ *   *
+ *    * Return: Always 0 (success)
+ *     */
+
+int main(void)
+{
+	        printf("with proper grammar, but the outcome is a piece of art,\n");
+		        return (0);
+}
