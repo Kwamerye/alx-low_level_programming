33463b037864d82ffdd4aada989007344cf64637
Richard Dzadey <rye2khosby@gmail.com>
2022-09-08 16:50:47 -0700

    Hello World C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 /**
  *
  * main - print the string in the put function
@@ -11,6 +10,6 @@
  */
 int main(void)
 {
-	puts("with proper grammar, but the outcome is a piece of art,\n");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
