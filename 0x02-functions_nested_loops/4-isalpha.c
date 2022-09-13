6981fa0027dd26f8a3c50213c70aaaa152caf0ba
Richard Dzadey <rye2khosby@gmail.com>
2022-09-13 15:00:40 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 
 /**
  * _isalpha - This  function is to check if c is a letter, lowercase or uppercase
- * @c: is the int that will be used for the argument of the function
+ * @c: is the int that will use for the argument of the function
  * Return: 0
  */
 
