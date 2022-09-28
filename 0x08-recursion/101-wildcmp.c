ac64c973d16d1267ecbc5fed1bec2118d144b9e3
Richard Dzadey <rye2khosby@gmail.com>
2022-09-28 11:04:39 -0700

    101-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * wildcmp - Entry Point
+ * @s1: input
+ * @s2: input
+ *
+ * Return: Always 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	return (0);
+}
