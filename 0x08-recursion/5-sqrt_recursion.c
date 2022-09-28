b073cbb9e6522393f2647bc6534cc9281b0c944a
Richard Dzadey <rye2khosby@gmail.com>
2022-09-28 10:48:07 -0700

    6-is_prime_number.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -14,7 +14,7 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 		return (-1);
 	else
-		return (squareroot(n, (n + 1) / 2));
+		return (_sqrt_recursion(n, (n + 1) / 2));
 }
 /**
  * squareroot - checks if perfect sqaure
@@ -31,5 +31,5 @@ int _sqrt_recursion(int n)
 	else if (i * i == n)
 		return (i);
 	else
-		return (squareroot(n, i - 1));
+		return (_sqrt_recursion(n, i - 1));
 }
