72cf09f7101319832293e7d8d50476e3f12832f6
Richard Dzadey <rye2khosby@gmail.com>
2022-09-30 13:57:03 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -5,7 +5,6 @@
 /**
  * main - Prints the addition of positive numbers,
  *        followed by a new line.
- * 
  * @argc: The number of arguments passed to the program.
  * @argv: An array of pointers to the arguments.
  *
