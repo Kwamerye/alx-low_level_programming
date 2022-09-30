c5f3b977d04ff12d124b2c36fadc322145a1b8bc
Richard Dzadey <rye2khosby@gmail.com>
2022-09-30 12:46:34 -0700

    Initial Commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the program name, followed by a new line.
+ * @argc: The number the arguement supplied to the program.
+ * @argv: An array of thepointer to the arguement.
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+
+	return (0);
+}
