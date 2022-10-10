3157e1752d5d4eb815d4c399dda0a24e8046a792
Richard Dzadey <rye2khosby@gmail.com>
2022-10-10 11:55:57 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * free_dog - Function that frees dogs.
+ * @d: struct to free
+ *
+ * Return: 0
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d);
+}
