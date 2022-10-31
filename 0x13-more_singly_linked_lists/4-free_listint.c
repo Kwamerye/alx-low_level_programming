42da2e5dc5fad11a5837a86d0f8dc48883ff2ef9
Richard Dzadey <rye2khosby@gmail.com>
2022-10-31 16:51:19 +0000

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -7,12 +7,12 @@
  */
 void free_listint(listint_t *head)
 {
-	listint_t *tmp;
+	listint_t *next;
 
 	while (head)
 	{
-		tmp = head->next;
+		next = head->next;
 		free(head);
-		head = tmp;
+		head = next;
 	}
 }
